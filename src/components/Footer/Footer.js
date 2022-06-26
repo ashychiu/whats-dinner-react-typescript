96c7f75714e8cac95407938009befe8be5b1b97f
ashychiu <ashychiu@gmail.com>
2022-06-26 16:06:31 +0800

    Initializing react app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a
+        href="https://github.com/ashychiu"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Created by Ashley Chiu © 2022
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
